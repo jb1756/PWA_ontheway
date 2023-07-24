885885027cca520d49cbde5004ba7ae9f1370d00
jb1756 <juliusbags04@yahoo.com>
2023-07-23 22:54:51 -0400

    db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {;
+  try {
+    const contactDb = await openDB('jate, 1');
+    const tx = contactDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add ({ content });
+  } catch (error) {
+    console.error('Error in Adding Content to the DB', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
 
+    const allContent = await store.getAll(); // Retreives all acontent
+    return allContent;
+  } catch (error) {
+    console.error('Error retrieving content from the database:', error);
+    return [];
+  }
+};
 initdb();
